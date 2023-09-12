1a27780c09fe02cd879eaabe386027bee3fd51d5
Christian Mancini <mancinichristian00@gmail.com>
2023-09-12 15:38:46 +0200

    Aggiunto il metodo per marcare le basi da togliere

--- HB_Spline.py
+++ HB_Spline.py
@@ -7,15 +7,15 @@ from matplotlib import pyplot as plt
 class HB_Spline():
     def __init__(self,mother:B_Spline) -> None:
         self.mother = mother.compute_base()
-        self.domains = [{"start":mother.knots[0],"stop":mother.knots[-1],"knots":len(mother.knots)}]
+        self.domains = [{"start":mother.knots[0],"stop":mother.knots[-1],"knots":len(mother.knots),"start_idx":0,"stop_idx":len(mother.knots)}]
         self.vectors = [{"knots":mother.knots,"level":0}]
-        self.level_basis = [{"basis":mother.get_base(),"level":0}]
+        self.level_basis = [{"b_spline":mother,"basis":mother.get_base(),"level":0}]
 
     def refine(self,range:tuple):
         self.add_domain(range)
         self.add_vector()
         self.compute_level_base()
-        self.compute_hierarchical_base()
+        self.mark_basis()
     
     def add_domain(self,range:tuple) -> None:
         start_idx,stop_idx = self.find_closest_range(range)
@@ -58,20 +58,42 @@ class HB_Spline():
         base = B_Spline(
             knots=self.vectors[-1]["knots"],
             order = self.mother.order
-        ).compute_base().get_base()
+        ).compute_base()
 
         level_base = {
-            "basis":base,
+            "b_spline":base,
+            "basis":base.get_base(),
             "level":self.vectors[-1]["level"]
         }
         self.level_basis.append(level_base)
 
-    def compute_hierarchical_base(self)->None:
-        self.remove_basis_from_previus_level()
+    def mark_basis(self) -> HB_Spline:
+        marked_basis = list()
+        #Si prende l'indice di inizio e fine rifinitura
+        level_domain_start = self.domains[-2]["start_idx"]
+        level_domain_stop = self.domains[-2]["stop_idx"]
 
-    def remove_basis_from_previus_level()->None:
-        rifine_domain_index = tuple(
-        ) 
+        #Si prende il numero di funzioni di base del livello corrente
+        number_basis_function = self.level_basis[-2]["b_spline"].number_basis_function
+
+        for i in range(number_basis_function):
+            base_domain_start = self.level_basis[-2]["b_spline"].domain_range[i]["start_idx"]
+            base_domain_stop = self.level_basis[-2]["b_spline"].domain_range[i]["stop_idx"]
+            print(f"-----{i}------")
+            print(f"start dominio: {level_domain_start} ,base: {base_domain_start}")
+            print(f"stop dominio: {level_domain_stop} ,base: {base_domain_stop}")
+
+            if (base_domain_start >= level_domain_start) and (base_domain_stop <= level_domain_stop):
+                marked_basis.append(1)
+                print(1)
+            else:
+                marked_basis.append(0)
+                print(0)
+
+        print(marked_basis)
+
+
+
 
 
     def plot_level_basis(self) -> None:
@@ -96,12 +118,14 @@ def main():
     T = np.linspace(0, 1, 7 + 1)
     mother = B_Spline(T, 3)
     hb = HB_Spline(mother)
-    hb.refine((0.2,0.8))
-    hb.refine((0.4,0.5))
-    print(hb.domains)
-    print(hb.vectors)
-    # hb.plot_level_basis()
-    # plt.show()
+    hb.refine((0.1,0.9))
+    hb.refine((0.3,0.7))
+    hb.refine((0.4,0.6))
+    # print(hb.domains)
+    # print(hb.vectors)
+    #print(hb.level_basis[0]["b_spline"].domain_range[0]["start_idx"])
+    hb.plot_level_basis()
+    plt.show()
     
 
 if __name__ == "__main__":
