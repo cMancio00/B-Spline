cd483c036b4152df89bc2800be21cfe4454bb253
Christian Mancini <mancinichristian00@gmail.com>
2023-09-12 16:11:59 +0200

    Cambiate le firme dei metodi per usare la fluent interface

--- HB_Spline.py
+++ HB_Spline.py
@@ -10,14 +10,16 @@ class HB_Spline():
         self.domains = [{"start":mother.knots[0],"stop":mother.knots[-1],"knots":len(mother.knots),"start_idx":0,"stop_idx":len(mother.knots)}]
         self.vectors = [{"knots":mother.knots,"level":0}]
         self.level_basis = [{"b_spline":mother,"basis":mother.get_base(),"level":0}]
+        self.marked_basis = list()
 
     def refine(self,range:tuple):
-        self.add_domain(range)
-        self.add_vector()
-        self.compute_level_base()
-        self.mark_basis()
+        self.\
+            add_domain(range).\
+            add_vector().\
+            compute_level_base().\
+            mark_basis()
     
-    def add_domain(self,range:tuple) -> None:
+    def add_domain(self,range:tuple) -> HB_Spline:
         start_idx,stop_idx = self.find_closest_range(range)
         domain = \
         {
@@ -27,21 +29,23 @@ class HB_Spline():
         }
         self.update_previus_domain(start_idx,stop_idx)
         self.domains.append(domain)
+        return self
 
-    def update_previus_domain(self,start_idx,stop_idx) -> None:
+    def update_previus_domain(self,start_idx,stop_idx) -> HB_Spline:
         self.domains[-1].update(
             {
             "start_idx":start_idx,
             "stop_idx":stop_idx
             }
         )
+        return self
 
     def find_closest_range(self,range:tuple) -> tuple[int,int]:
         start_idx = (np.abs(self.vectors[-1]["knots"] - range[0])).argmin()
         stop_idx = (np.abs(self.vectors[-1]["knots"] - range[1])).argmin()
         return start_idx, stop_idx
     
-    def add_vector(self) -> None:
+    def add_vector(self) -> HB_Spline:
         knots = np.linspace(
             self.domains[0]["start"],
             self.domains[0]["stop"],
@@ -53,8 +57,9 @@ class HB_Spline():
             "level":len(self.vectors)
         }
         self.vectors.append(vector)
+        return self
 
-    def compute_level_base(self) -> None:
+    def compute_level_base(self) -> HB_Spline:
         base = B_Spline(
             knots=self.vectors[-1]["knots"],
             order = self.mother.order
@@ -66,6 +71,7 @@ class HB_Spline():
             "level":self.vectors[-1]["level"]
         }
         self.level_basis.append(level_base)
+        return self
 
     def mark_basis(self) -> HB_Spline:
         marked_basis = list()
@@ -79,21 +85,14 @@ class HB_Spline():
         for i in range(number_basis_function):
             base_domain_start = self.level_basis[-2]["b_spline"].domain_range[i]["start_idx"]
             base_domain_stop = self.level_basis[-2]["b_spline"].domain_range[i]["stop_idx"]
-            print(f"-----{i}------")
-            print(f"start dominio: {level_domain_start} ,base: {base_domain_start}")
-            print(f"stop dominio: {level_domain_stop} ,base: {base_domain_stop}")
 
             if (base_domain_start >= level_domain_start) and (base_domain_stop <= level_domain_stop):
                 marked_basis.append(1)
-                print(1)
             else:
                 marked_basis.append(0)
-                print(0)
 
-        print(marked_basis)
-
-
-
+        self.marked_basis.append(marked_basis)
+        return self
 
 
     def plot_level_basis(self) -> None:
@@ -121,11 +120,13 @@ def main():
     hb.refine((0.1,0.9))
     hb.refine((0.3,0.7))
     hb.refine((0.4,0.6))
+    print(hb.marked_basis)
+
     # print(hb.domains)
     # print(hb.vectors)
     #print(hb.level_basis[0]["b_spline"].domain_range[0]["start_idx"])
     hb.plot_level_basis()
-    plt.show()
+    #plt.show()
     
 
 if __name__ == "__main__":
